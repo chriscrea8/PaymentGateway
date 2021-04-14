69cad18ff35e10ca66df4afc2a9539a2361ebfac
Christian Chukwu <52824325+chriscrea8@users.noreply.github.com>
2021-04-14 19:14:31 +0100

    Add files via upload
    Included modal for successful payment

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 // Get modal elements
 var modal = document.getElementById('easyswitch-simpleModal');
 
+var modalContent = document.querySelector('.easyswitch-modal-content');
+
 //Get open modal button
 var modalBtn = document.getElementById('easyswitch-modalBtn');
 
 //Get close button
 var closeBtn = document.getElementsByClassName('easyswitch-closeBtn')[0];
+
+//Get success page
+var success = document.querySelector('.easyswitch-modal-success');
+
+//Get secure pay submit button
+var submitBtn = document.querySelector('.easyswitch-submit');
+
+//Get success dismiss button
+var dismissBtn = document.getElementById('easyswitch-modal-dismiss-button');
 
 //Listen for open click
 modalBtn.addEventListener('click', openModal);
@@ -16,6 +27,19 @@ closeBtn.addEventListener('click', closeModal);
 //Listen for outside click
 // window.addEventListener('click', clickOutside);
 
+//Listen for submit button click
+submitBtn.addEventListener('click', function(){
+    modalContent.style.display = "none";
+    closeBtn.style.display = "none";
+    success.classList.add("active");
+}); 
+
+//Listen for dismiss button click
+dismissBtn.addEventListener('click', function(){
+    success.classList.remove("active");
+    modal.style.display = "none";
+    location.reload();
+}); 
 
 //function to open modal
 function openModal(){
@@ -64,3 +88,5 @@ $(document).ready(function() {
     });
     
 });
+
+
